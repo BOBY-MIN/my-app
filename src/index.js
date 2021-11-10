6b20bcdb33c659b47ec2748b42b48c09f4818956
bobypoby123@gmail.com <@alsgmlalsdl16>
2021-11-10 10:34:59 +0900

    리액트 자습서 따라하기 완료

--- src/index.js
+++ src/index.js
@@ -29,47 +29,54 @@ import "./index.css";
   
   class Board extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
+    // Game 컴포넌트에서 props 를 주입받는 방식으로 변경
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true,
+    //     };
+    // }
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if(calculateWinner(squares) || squares[i]){
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
-        });
-    }
+    // Game 컴포넌트에서 props 를 주입받는 방식으로 변경
+    // handleClick(i) {
+    //     const squares = this.state.squares.slice();
+    //     if(calculateWinner(squares) || squares[i]){
+    //       return;
+    //     }
+    //     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //     this.setState({
+    //       squares: squares,
+    //       xIsNext: !this.state.xIsNext,
+    //     });
+    // }
 
     renderSquare(i) {
       return (
-            <Square 
-                value={this.state.squares[i]} 
-                onClick={() => this.handleClick(i)}
+            <Square
+                // Game 컴포넌트에서 props 를 주입받는 방식으로 변경 
+                // value={this.state.squares[i]} 
+                // onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]} 
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if(winner){
-        status = 'Winner: ' + winner;
-      }else{
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
+      // Game 컴포넌트에서 props 를 주입받는 방식으로 변경 
+      // const winner = calculateWinner(this.state.squares);
+      // let status;
+      // if(winner){
+      //   status = 'Winner: ' + winner;
+      // }else{
+      //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      // }
   
       return (
         <div>
-          <div className="status">{status}</div>
+          {/* Game 컴포넌트에서 props 를 주입받는 방식으로 변경  */}
+          {/* <div className="status">{status}</div> */}
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -91,15 +98,78 @@ import "./index.css";
   }
   
   class Game extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true,
+        stepNumber: 0,
+      }
+    }
+
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      })
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      })
+    }
+
     render() {
+
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          move + '단계로 이동' :
+          '시작하기';
+
+        return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+        );
+      });
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
